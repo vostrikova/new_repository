39bab2a17f92b4c4e489c1372b5f949d8a8fbc48
Cheburek <lera_vostrikova@mail.ru>
2017-09-20 19:59:37 +0300

    new_commit

--- cheburek.py
+++ cheburek.py
@@ -1,5 +1,9 @@
 print('абсолютно бесполезный говнокод')
 
 
-def top_func():
-    print('делаем некоторые изменения')
+def top_func(a):
+    a.reverse()
+    print('делаем некоторые изменения', a)
+
+top_func([1, 2, 3])
+print('что happening')
